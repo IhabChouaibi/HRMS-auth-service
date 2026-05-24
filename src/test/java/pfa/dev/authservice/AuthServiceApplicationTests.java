9fdb7493159b1f3161b254d903bbfb266ac020e0
IhabChouaibi <ihebch315@gmail.com>
2026-05-24 13:52:38 +0100

    test fixed

--- src/test/java/pfa/dev/authservice/AuthServiceApplicationTests.java
+++ src/test/java/pfa/dev/authservice/AuthServiceApplicationTests.java
@@ -2,12 +2,16 @@ package pfa.dev.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AuthServiceApplicationTests {
 
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
     @Test
     void contextLoads() {
     }
-
 }
